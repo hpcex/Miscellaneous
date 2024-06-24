d7648420ca595ea68abc46a6623c56882ae25e40
hpcex <hpcex53@gmail.com>
2024-06-24 21:01:34 +0800

    Update eza.sh

--- eza/eza.sh
+++ eza/eza.sh
@@ -3,4 +3,5 @@ apt install -y gpg
 mkdir -p /etc/apt/keyrings
 wget -qO- https://raw.githubusercontent.com/eza-community/eza/main/deb.asc | gpg --dearmor -o /etc/apt/keyrings/gierens.gpg
 echo "deb [signed-by=/etc/apt/keyrings/gierens.gpg] http://deb.gierens.de stable main" | tee /etc/apt/sources.list.d/gierens.list
+apt update
 apt install -y eza
