760217bdc35761946b2ce7874dfce8c7a7d67f6b
hpcex <hpcex53@gmail.com>
2024-07-12 11:29:46 +0800

    Update eza.sh

--- eza/eza.sh
+++ eza/eza.sh
@@ -1,7 +1,8 @@
-apt update
 apt install -y gpg
 mkdir -p /etc/apt/keyrings
 wget -qO- https://raw.githubusercontent.com/eza-community/eza/main/deb.asc | gpg --dearmor -o /etc/apt/keyrings/gierens.gpg
-echo "deb [signed-by=/etc/apt/keyrings/gierens.gpg] http://deb.gierens.de stable main" | tee /etc/apt/sources.list.d/gierens.list
+echo "deb [signed-by=/etc/apt/keyrings/gierens.gpg] http://deb.gierens.de stable main" | tee -a /etc/apt/sources.list
 apt update
 apt install -y eza
+echo -e 'alias ls="eza --icons"\nalias ll="eza --time-style=long-iso --icons --binary -lhg"\nalias tree="eza --tree --icons"' >> ~/.bashrc
+source .bashrc
